1db055cbd70c66a165bbbfe3f003573ae1bed926
Rajesh <dominuscrystal@gmail.com>
2025-07-26 20:35:01 +0530

    Basic Start

---
+++ Scripts/DestroyObjectX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using Unity.UI;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+
+}
